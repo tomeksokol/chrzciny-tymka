6b235ac37f02b63b14456239a8af4b8a59e3f97e
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-08-27 21:48:45 +0200

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// library import
+//import * as basicLightbox from 'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js';
 
 console.log(galleryItems);
-
-// const markup = galleryItems
-// .map((galleryItem) => `<ul class="list"> <li class="preview". ${galleryItem.preview}</li> </ul>`);
-// .join("");
-// console.log(markup);
-
-// list.innerHTML = markup;
 
 const galleryContainer = document.querySelector('.gallery');
 console.log(galleryContainer);
@@ -19,13 +14,43 @@ galleryContainer.append(ul);
 console.log(ul);
 
 const galleryList = document.querySelector('.list');
+console.log(galleryList.children);
 
-const listOfImages = galleryItems
-  .map(
-    galleryItem =>
-      `<li class="item"> <img class="preview" src="${galleryItem.preview}" loading="lazy" alt="${galleryItem.description}"></li>`
-  )
-  .join('');
-console.log(listOfImages);
+const listOfImagesPreview = galleryItems.map(
+  galleryItem =>
+    `<li class="item"> <img class="preview" src="${galleryItem.preview}" loading="lazy" alt="${galleryItem.description}"></li>`
+);
+console.log(listOfImagesPreview);
+galleryList.insertAdjacentHTML('beforeend', listOfImagesPreview);
 
-galleryList.insertAdjacentHTML('beforeend', listOfImages);
+const listOfImagesOriginal = galleryItems.map(
+  galleryItem =>
+    `<li class="item"> <img class="original" src="${galleryItem.original}" loading="lazy" alt="${galleryItem.description}"></li>`
+);
+console.log(listOfImagesOriginal);
+galleryList.insertAdjacentHTML('beforeend', listOfImagesOriginal);
+
+const hiddenClass = document.querySelectorAll('.original');
+hiddenClass.style.display = 'none';
+
+const images = document.querySelectorAll('img');
+console.log(images);
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    basicLightbox
+      .create(
+        `<img width="1400" height="900" src="${image.src}" loading="lazy" alt="${galleryItems.src}">`
+      )
+      .show();
+  });
+});
+
+// galleryContainer.addEventListener('click', event => {
+//   galleryItems.map(galleryItem =>
+//     basicLightbox
+//       .create(
+//         `<img width="1400" height="900" src="${galleryItem.original}" loading="lazy" alt="${galleryItem.description}">`
+//       )
+//       .show()
+//   );
+// });
