54f889a7a350c62472f0dac9f675100c904d5cfe
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-08-28 12:49:41 +0200

    4

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,15 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+console.log(galleryContainer);
+
+const photoContainer = galleryItems
+  .map(
+    galleryItem =>
+      `<a class="gallery__item" href="${galleryItem.original}"> <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"/></a>`
+  )
+  .join('');
+galleryContainer.insertAdjacentHTML('beforeend', photoContainer);
+console.log(photoContainer);
